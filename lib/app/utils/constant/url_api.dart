c0ab5718cd4778a4d69f9023c275018f064b9e45
Victor <deyanovitch77580@gmail.com>
2021-05-08 16:01:52 +0200

    Integrate Default CRUD API on Firebase

---
+++ lib/app/utils/constant/url_api.dart
@@ -0,0 +1,7 @@
+abstract class UrlAPI {
+
+  // FIREBASE FUNCTION API
+    static final String readBookID =
+        'https://europe-west3-book-app-7f51e.cloudfunctions.net/app/api/readBookID/';
+
+}
