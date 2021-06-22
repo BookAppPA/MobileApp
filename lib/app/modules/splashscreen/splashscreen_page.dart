3772551896fa64002f51971dff97fcd5b6ff7879
Victor-Dev77 <deyanovitch77580@gmail.com>
2021-06-22 14:17:44 +0200

    Change App Logo + App Name

--- lib/app/modules/splashscreen/splashscreen_page.dart
+++ lib/app/modules/splashscreen/splashscreen_page.dart
@@ -1,25 +1,18 @@
+import 'package:book_app/app/utils/constant/constant_image.dart';
 import 'package:flutter/material.dart';
-//import 'package:flutter_svg/svg.dart';
 
 class SplashScreenPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
       body: Center(
         child: Container(
-          height: 109,
+          height: 100,
           width: 100,
-          child: FlutterLogo(),
-         // child:
-              /*SvgPicture.asset(
-            "asset/bomb_splashscreen.svg",
+          child: Image.asset(
+            ConstantImage.logo,
             fit: BoxFit.contain,
-          ),*/
-             /* Image.asset(
-            ConstantImage.bomb,
-            fit: BoxFit.contain,
-          ),*/
+          ),
         ),
       ),
     );
--- lib/app/utils/constant/constant_image.dart
+++ lib/app/utils/constant/constant_image.dart
@@ -2,13 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 abstract class ConstantImage {
-  static final interrogation = "asset/gold_interrogation.png";
-  static final moodSerious = "asset/icon_mood_serious.png";
-
-  static final bombrImage = Image.asset(
-    "asset/bombr_name_white.png",
-    fit: BoxFit.contain,
-  );
+  static final logo = "assets/logo.png";
 
   static final onBoarding1 = SvgPicture.asset(
     "assets/onboarding_1.svg",
